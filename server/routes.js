2151b6e45fd0d2c9009d847232978089969dca51
Ali Al Dallal <ali@alicoding.com>
2014-05-29 14:44:48 -0400

    Add new get method

--- server/routes.js
+++ server/routes.js
@@ -2,86 +2,49 @@
 // We have to figure out how to deal with binary vs. json data,
 // file uploads, etc.
 
-module.exports = function( knoxClient ) {
+var version = require( "../package" ).version;
+var S3Provider = require("filer-s3");
+var Filer = require("filer");
+var mime = require("mime");
 
-  var version = require( "../package" ).version;
+function getFileInfo(path) {
+  return {
+    mime: mime.lookup(path),
+    encoding: mime.charsets.lookup(mime.lookup(path)) || null
+  }
+}
 
-  function jsonError( res, code, msg, err ) {
-    res.json( code, {
-      msg: msg,
-      err: err
+module.exports = {
+
+  index: function( req, res ) {
+    res.send( "MakeDrive: https://wiki.mozilla.org/Webmaker/MakeDrive" );
+  },
+  get: function( req, res ) {
+    var user = req.session.user;
+    var path = req.params[0];
+    var info = getFileInfo(path);
+
+    var fs = new Filer.FileSystem({provider: new S3Provider({keyPrefix: user.username, name: user.username, bucket: "<bucket>", key: "<key>", secret: "<secret>"})});
+    fs.readFile("/" + path, info.encoding, function(err, data) {
+      if(err) {
+        if(err.code === "ENOENT") {
+          res.send(404);
+          return;
+        } else {
+          res.send(500, "Unable to read file" + err);
+        }
+      }
+      res.writeHead(200, "OK", {'Content-Type': info.mime});
+      res.write(data);
+      res.end()
+    });
+  },
+  healthcheck: function( req, res ) {
+    res.json({
+      http: "okay",
+      version: version
     });
   }
-
-  return {
-
-    get: function( req, res ) {
-      // TODO: figure out proper errors to bubble up, see:
-      // http://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html
-      knoxClient.get( req.params.key )
-        .on( "error",  done )
-        .on( "response", function( response ) {
-          // TODO: figure out right way to return the data (json vs. binary)
-          var data = '';
-          response.on( "data", function( chunk ) {
-            data += chunk;
-          }).on( "end", function() {
-            res.json( data );
-          }).on('error', function( err ) {
-            jsonError( res, 400, "Unable to get value.", err );
-          });
-        })
-        .end();
-    },
-
-    put: function( req, res ) {
-      // TODO: deal with binary data, file parts, ???
-      // TODO: figure out proper errors to bubble up, see:
-      // http://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html
-      var data = req.body.value,
-          headers = {
-            'x-amz-acl': 'public-read', // TODO: not sure if we need this...
-            'Content-Length': Buffer.byteLength( data,'utf8' ),
-            'Content-Type': 'text/plain;charset=UTF-8'
-          };
-
-      knoxClient.put( req.params.key, headers )
-        .on( "error", function( err ) {
-          jsonError( res, 500, "Error storing value", err );
-        })
-        .on( "response", function(res) {
-          if (res.statusCode !== 200) {
-            return jsonError( res, 500, "Error storing value", err );
-          }
-          res.json( 200 );
-        })
-        .end( data );
-    },
-
-    del: function( req, res ) {
-      // TODO: figure out proper errors to bubble up, see:
-      // http://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html
-      knoxClient.del( req.params.key )
-        .on( "error", function( err ) {
-          jsonError( res, 500, "Error removing key/value pair", err );
-        })
-        .on( "response", function(res) {
-          if (res.statusCode !== 200) {
-            return jsonError( res, 500, "Error removing key/value pair", err );
-          }
-          res.json( 200 );
-        })
-        .end();
-    },
-
-    healthcheck: function( req, res ) {
-      res.json({
-        http: "okay",
-        version: version
-      });
-    }
-
-    // TODO: do we want clear?
 
   };
 };
