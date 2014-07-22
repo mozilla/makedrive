0979da44bae28d11e6390286c3132dbe3ac2a6d2
yoavgurevich <ygurevich@ymail.com>
2014-07-22 14:54:09 -0400

    initial logic input for query strings

--- server/routes/middleware/index.js
+++ server/routes/middleware/index.js
@@ -10,14 +10,21 @@ function generateError( code, msg ) {
 module.exports = {
   authenticationHandler: function( req, res, next ) {
     var username = req.session && req.session.user && req.session.user.username;
+    
+    // If cookie session ain't present, see if data got passed in via query string
     if ( !username ) {
-      return next( generateError( 401, "Webmaker Authentication Required." ) );
+      if(req.query.username){
+        username = req.query.username;
+      }
+      else {
+        return next( generateError( 401, "Webmaker Authentication Required." ) );
+      }
     }
-
+    
     req.session.sessionId = websocketAuth.createSessionTracker(username, req.session.sessionId);
-
-    req.params.username = username;
     req.params.sessionId = req.session.sessionId;
+    req.params.username = username;
+    
     next();
   },
   crossOriginHandler: function( req, res, next ) {
