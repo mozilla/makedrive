5a2e5bafcd833e6b19bd94498e9ee7a60bfac6c7
Ali Al Dallal <ali@alicoding.com>
2014-10-20 14:19:37 -0400

    Add filer-sql support

--- server/lib/filesystem.js
+++ server/lib/filesystem.js
@@ -13,6 +13,13 @@ if ( providerType === "filer-s3" ) {
   defaults.bucket = env.get( "S3_BUCKET" );
   defaults.key = env.get( "S3_KEY" );
   defaults.secret = env.get( "S3_SECRET");
+} else if ( providerType === "filer-sql" ) {
+  defaults.type = Provider[env.get( "DB_TYPE" )];
+  defaults.db = {
+    name: env.get( "DB_NAME" ),
+    username: env.get( "DB_USERNAME" ),
+    password: env.get( "DB_PASSWORD" )
+  };
 }
 
 module.exports = {
@@ -20,6 +27,8 @@ module.exports = {
     delete cachedFS[name];
   },
   create: function( options ) {
+    // in filer-sql we are expecting option 'user' which is the same as 'options.name'
+    defaults.user = providerType === "filer-sql" ? options.name : "";
     Object.keys( defaults ).forEach(function( defaultOption ) {
       options[ defaultOption ] = options[ defaultOption ] || defaults[ defaultOption ];
     });
